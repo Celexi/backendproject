b555208ff4b060b8bcf90f85134f733d02305115
celexi <celexi@bytenix.net>
2018-05-02 18:22:30 +0100

    as

--- routes/listperson.js
+++ routes/listperson.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 var bodyParser = require('body-parser');
-var file1 = require('\Users\Celex\backendproject\code\backendproject\videos.json')
-import file1 from 'C:\Users\Celex\backendproject\code\backendproject\videos.json'
-router.get('/listPerson', function(req, response){
 
-    response.send(file1);
+
+
+router.get('/listPerson', function(req, response){
+    var file = fs.readFileSync("C:\\Users\\Celex\\backendproject\\code\\backendproject\\videos.json");
+    response.send(file);
 
 }
 
