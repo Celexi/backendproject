c64103359a1297d79d3844d24d63950cf1b09f92
celexi <celexi@bytenix.net>
2018-05-02 18:40:19 +0100

    ghgjbn

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var listpersonRouter= require('routes/listperson');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/listperson', listpersonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/listperson.js
+++ routes/listperson.js
@@ -3,12 +3,19 @@ var router = express.Router();
 const fs = require('fs');
 var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
+
+function readFile(path){
+    var file = fs.readFileSync(path, 'utf-8');
+    return file;
+}
 
 router.get('/listPerson', function(req, response){
     var file = fs.readFileSync("videos.json");
-    response.send(file);
+    response.json(file);
 
-}
+})
 
 module.exports = router;
